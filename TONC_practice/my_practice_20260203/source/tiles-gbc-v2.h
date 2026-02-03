23010e65c253254e5238b28e80302b1eef68c046
Tran Le Phuong Lan <l.p.l.tran@tue.nl>
2026-02-03 17:26:33 +0100

    draw_tiles progress 20260203 	- progress: 	  1. Adopt the Makefile to directly include data from assembly file (./source/tiles-gbc-v2.s) and its definition (./tiles-gbc-v2.h) into the final .gba program 	  2. the data from assembly file (./source/tiles-gbc-v2.s) and its definition (./tiles-gbc-v2.h) are exact copied from ../my_practice_20260126/build/tiles-gbc-v2.s and ../my_practice_20260126/build/tiles-gbc-v2.h 	  3. Except for the Makefile, the source code ./source/draw_tile_demo.c is the exact copied from ../my_practice_20260126/source/draw_tile_demo.c 	- Files involved: 		new file:   TONC_practice/my_practice_20260203/Makefile 		new file:   TONC_practice/my_practice_20260203/source/draw_tile_demo.c 		new file:   TONC_practice/my_practice_20260203/source/tiles-gbc-v2.h 		new file:   TONC_practice/my_practice_20260203/source/tiles-gbc-v2.s

---
+++ TONC_practice/my_practice_20260203/source/tiles-gbc-v2.h
@@ -0,0 +1,31 @@
+
+//{{BLOCK(tiles_gbc_v2)
+
+//======================================================================
+//
+//	tiles_gbc_v2, 216x8@4, 
+//	+ palette 8 entries, not compressed
+//	+ 27 tiles not compressed
+//	Total size: 16 + 864 = 880
+//
+//	Time-stamp: 2026-01-26, 21:37:23
+//	Exported by Cearn's GBA Image Transmogrifier, v0.9.2
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_TILES_GBC_V2_H
+#define GRIT_TILES_GBC_V2_H
+
+#define tiles_gbc_v2TilesLen 864
+extern const unsigned int tiles_gbc_v2Tiles[216];
+
+#define tiles_gbc_v2PalLen 16
+extern const unsigned short tiles_gbc_v2Pal[8];
+
+#define tiles_gbc_v2MapLen 288
+extern const unsigned int tiles_gbcMap_v2[72];
+
+#endif // GRIT_TILES_GBC_V2_H
+
+//}}BLOCK(tiles_gbc_v2)
